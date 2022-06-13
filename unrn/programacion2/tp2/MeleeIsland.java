9d98c0932626cf9aa4c5df2176eb3cfe01878a9d
Hanchut <Galoorellana2@hotmail.com>
2022-06-13 12:52:38 -0300

    hola 2

--- unrn/programacion2/tp2/MeleeIsland.java
+++ unrn/programacion2/tp2/MeleeIsland.java
@@ -4,14 +4,14 @@ import java.util.*;
 
 public class MeleeIsland {
 
-    private TreeMap<String, Pirata> Info_pirata;
+    private TreeMap<String, InfoPirata> Info_pirata;
     private TreeMap<String, Grog> grogs;
     private TreeSet<String> piratas;
     private String mas_buscapleitos;
     private String mas_picante;
     private String proximo_grog;
 
-    public TreeMap<String, Pirata>Info_pirata(){
+    public TreeMap<String, InfoPirata>Info_pirata(){
         return Info_pirata;
     }
 
@@ -19,20 +19,11 @@ public class MeleeIsland {
         return piratas;
     }
 
-    public void agregarPirata(String pirata, TreeSet<Insulto> insultos){
-        if(piratas.contains(pirata)){
-            throw new IllegalStateException("El pirata que se desea agregar ya esta en el diccionario.");
-        } else {
-            this.piratas.add(pirata);
-            this.Info_pirata.put(pirata, new Pirata(pirata, insultos));
-        }
-    }
-
-    public String masBuscapleitos(){
+    public String masBuscapleitos(){ // for each
         return mas_buscapleitos;
     }
 
-    public String masPicante(){
+    public String masPicante(){ // for each --> secuencias de peleas (true)
         return mas_picante;
     }
 
@@ -44,13 +35,40 @@ public class MeleeIsland {
         return grogs;
     }
 
-    public MeleeIsland empezar(){
+    public MeleeIsland empezar(TreeMap<Pirata>){
 
+    }
+
+    public void agregarPirata(String pirata, TreeSet<Insulto> insultos){
+        if(piratas.contains(pirata)){
+            throw new IllegalStateException("El pirata que se desea agregar ya esta en el diccionario.");
+        } else {
+            this.piratas.add(pirata);
+            this.Info_pirata.put(pirata, new InfoPirata(pirata, insultos));
+        }
     }
 
     public MeleeIsland beberGrog(){
 
     }
 
-    
+    public MeleeIsland pelear(){
+
+    }
+
+    public MeleeIsland peleas(){
+
+    }
+
+    public MeleeIsland grogsBebidos(){
+
+    }
+
+    public MeleeIsland alcoholEnSangre(){
+
+    }
+
+    public MeleeIsland insultosQueConoce(){
+
+    }
 }
